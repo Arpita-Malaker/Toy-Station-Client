61b83ae25e6cfbf867533049b82f9613574f55e6
Arpita-Malaker <arpita.uiu@gmail.com>
2023-06-15 14:09:00 +0600

    social media button

--- src/Pages/Contact/SocialLogin/SocialLogin.jsx
+++ src/Pages/Contact/SocialLogin/SocialLogin.jsx
@@ -2,9 +2,17 @@
 
 const SocialLogin = () => {
     return (
-        <div>
+        <div className=" social-button-container w-50 mt-3">
+        <div className="">
+          <img
             
+            className=" social-button"
+            src="https://i.ibb.co/gSTHXZJ/google-btn.png"
+            alt=""
+          />
         </div>
+       
+      </div>
     );
 };
 
