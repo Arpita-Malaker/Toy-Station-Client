b39034abae28048abdb46f36986abca0aae709aa
Arpita-Malaker <arpita.uiu@gmail.com>
2023-06-15 11:44:37 +0600

    gallary

--- src/Pages/Gellary/Gellary.jsx
+++ src/Pages/Gellary/Gellary.jsx
@@ -1,11 +1,90 @@
 
+import p1 from '../../assets/gellary/1.jpg'
+import p2 from '../../assets/gellary/2.jpg'
+import p3 from '../../assets/gellary/3.jpg'
+import p4 from '../../assets/gellary/4.jpg'
+import p5 from '../../assets/gellary/5.jpg'
+import p6 from '../../assets/gellary/6.jpg'
+import p7 from '../../assets/gellary/7.jpg'
+import p8 from '../../assets/gellary/8.jpg'
+import p9 from '../../assets/gellary/9.jpg'
+import p10 from '../../assets/gellary/10.jpg'
+import p11 from '../../assets/gellary/11.jpg'
+import p12 from '../../assets/gellary/12.jpg'
+import p13 from '../../assets/gellary/13.jpg'
+import './Gellary.css'
 
 const Gellary = () => {
+
+    let data=[
+
+        {
+            id:1,
+            imgSrc:p11
+        },
+        {
+            id:2,
+            imgSrc:p2
+        },
+        {
+            id:3,
+            imgSrc:p3
+        },
+        {
+            id:4,
+            imgSrc:p4
+        },
+        {
+            id:5,
+            imgSrc:p5
+        },
+        {
+            id:6,
+            imgSrc:p6
+        },
+        {
+            id:7,
+            imgSrc:p7
+        },
+        {
+            id:8,
+            imgSrc:p8
+        },
+        {
+            id:9,
+            imgSrc:p9
+        },
+        {
+            id:10,
+            imgSrc:p10
+        },
+        {
+            id:11,
+            imgSrc:p1
+        },
+        {
+            id:12,
+            imgSrc:p12
+        }
+    ]
+  
     return (
-        <div>
-            <h2>hh</h2>
+       <div className='mt-16'>
+         <h2 className='text-3xl text-green-800 font-bold text-center'>Toy Gellary</h2>
+        <div className="gellary mt-16">
+           
+
+            {
+                data.map((item,index)=><div className='pics' key={index}>
+                    <img src={item.imgSrc}  />
+                </div>
+
+                )
+            }
+      
             
         </div>
+       </div>
     );
 };
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
 
   <div className="hero-content text-center ">
     <div className="max-w ">
-      <h1 className="mb-5 text-5xl font-bold">Wellcome to <span className=' text-yellow-500	 '>Toy Station</span></h1>
-      <p className="mb-5 ml-10 max-w-md">Welcome to Toy Station – your #1 hotspot for brilliant toys, infant items, diversions, kids’ toy and  music toy and more.
+      <h1 className="mb-5 text-5xl text-yellow-500 font-bold">Wellcome to <span className=' text-yellow-500	 '>Toy Station</span></h1>
+      <p className="mb-5 ml-10 max-w-md text-yellow-100">Welcome to Toy Station – your #1 hotspot for brilliant toys, infant items, diversions, kids’ toy and  music toy and more.
 
 We’re committed to giving you the plain best with attention to the most astounding quality, unwavering quality, client administration, and uniqueness.</p>
       <button className="btn btn-warning">See All ToyS</button>
